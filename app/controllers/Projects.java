d4de13c5718ea08ca5eb70ceba05fd0bec7c396a
Kanokphol Techarattanaprasert <sonnyhonny@gmail.com>
2012-12-09 06:56:53 +0700

    Update CRUD of Project class
    - Now: Create, Read, and Delete
    - Change views/project.scala.html to projects.scala.html (List of
    Projects)
    - Add new views/project.scala (Each Project's page)
    - Add routes for each Project

--- app/controllers/Projects.java
+++ app/controllers/Projects.java
@@ -12,18 +12,22 @@ public class Projects extends Controller {
 	static Form<Project> projectForm = form(Project.class);
 	
 	public static Result index() {
-		return ok(project.render(Project.find.all(), projectForm));
+		return ok(projects.render(Project.find.all(), projectForm));
+	}
+	
+	public static Result project(String name) {
+		return ok(project.render(Project.find.where().eq("name", name).findUnique()));
 	}
 	
 	public static Result create() {
 		Form<Project> insertForm = form(Project.class).bindFromRequest();
 		insertForm.get().save();
-		return ok(project.render(Project.find.all(), projectForm));
+		return ok(projects.render(Project.find.all(), projectForm));
 	}
 	
 	public static Result delete(Long id) {
 		Project.find.ref(id).delete();
-		return ok(project.render(Project.find.all(), projectForm));
+		return ok(projects.render(Project.find.all(), projectForm));
 	}
 	
 }

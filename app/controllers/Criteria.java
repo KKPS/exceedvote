bc2601368d095b43231703362c19f7a7c11df8f5
Kanokphol Techarattanaprasert <sonnyhonny@gmail.com>
2012-12-15 05:57:53 +0700

    Update CRUD of Criterion class
    - Now: Create, Read, Update, and Delete :)
    - Update controllers/Criteria.java
    - Add views/criterion.scala.html
    - Update Application routes

--- app/controllers/Criteria.java
+++ app/controllers/Criteria.java
@@ -10,13 +10,36 @@ import views.html.*;
 public class Criteria extends Controller {
 
 	public static Result index() {
-		return ok(views.html.criteria.render(Criterion.find.all()));
+		return ok(views.html.criteria.render(Criterion.find.all(), form(Criterion.class)));
+	}
+	
+	public static Result criterion(Long id) {
+		Criterion criterion = Criterion.find.ref(id);
+		Form<Criterion> editForm = form(Criterion.class);
+		if (criterion != null) {
+			editForm = editForm.fill(criterion);
+		}
+		return ok(views.html.criterion.render(criterion, editForm));
 	}
 	
 	public static Result create() {
 		Form<Criterion> createForm = form(Criterion.class).bindFromRequest();
 		createForm.get().save();
-		return ok(views.html.criteria.render(Criterion.find.all()));
+		return ok(views.html.criteria.render(Criterion.find.all(), form(Criterion.class)));
+	}
+	
+	public static Result update(Long id) {
+		Criterion criterion = Criterion.find.ref(id);
+		Form<Criterion> updateForm = form(Criterion.class).bindFromRequest();
+		Criterion updated = updateForm.get();
+		updated.update(criterion.id);
+		return ok(views.html.criterion.render(updated, updateForm));
+	}
+	
+	public static Result delete(Long id) {
+		Criterion criterion = Criterion.find.ref(id);
+		criterion.delete();
+		return redirect("/criterion");
 	}
 	
 }
